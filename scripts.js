8fdf8fafc96d9f12f124e4a0d319ec4ebf90aa50
Patrick Bower <bower.patrick@gmail.com>
2019-01-07 11:13:00 +0000

    First commit Test device light API

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-console.log("QuickStart - nothing works unless you do");
+navigator.mediaDevices.getUserMedia({
+  video: { facingMode: { exact: "environment" } }
+})
+.then((stream) => {
+  const video = document.querySelector('video');
+  video.srcObject = stream;
+  
+  // get the active track of the stream
+  const track = stream.getVideoTracks()[0];
+
+  video.addEventListener('loadedmetadata', (e) => {  
+    window.setTimeout(() => (
+      onCapabilitiesReady(track.getCapabilities())
+    ), 500);
+  });
+
+  function onCapabilitiesReady(capabilities) {
+    if (capabilities.torch) {
+      track.applyConstraints({
+        advanced: [{torch: true}]
+      })
+      .catch(e => console.log(e));
+    }
+  }
+})
+.catch(err => console.error('getUserMedia() failed: ', err));
