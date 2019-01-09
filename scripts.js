b23f105821ad3dc41c186cd1d1f2878bfb2c3952
Patrick Bower <bower.patrick@gmail.com>
2019-01-09 14:09:35 +0000

    led blinking correctly

--- scripts.js
+++ scripts.js
@@ -8,24 +8,42 @@ navigator.mediaDevices.getUserMedia({
   
   // get the active track of the stream
   const track = stream.getVideoTracks()[0];
+  let timeOutVar;
+  let blink = true;
 
   video.addEventListener('loadedmetadata', (e) => {  
-    window.setTimeout(() => (
-      onCapabilitiesReady(track.getCapabilities())
-    ), 500);
+    let timer = function(terminator = false) {
+      if(terminator) {
+          clearTimeout(timeOutVar);
+          onCapabilitiesReady(track.getCapabilities(), false);
+      } else {
+          onCapabilitiesReady(track.getCapabilities(), blink);
+          blink = !blink;
+          timeOutVar = setTimeout(function(){timer();}, 500);
+      }
+    }   
+    // timer(false); //  -> start loop
+
+    let stopBtn = document.querySelector(".stop-btn");
+    stopBtn.addEventListener("click", () => {
+      timer(true); //  -> end loop
+      console.log('stop you bugger');
+    })
+
+    let startBtn = document.querySelector(".start-btn");
+    startBtn.addEventListener("click", () => {
+      timer(false); //  -> start loop
+      console.log('get going');
+    })
+
   });
 
-  function onCapabilitiesReady(capabilities) {
+  function onCapabilitiesReady(capabilities, blink) {
+    console.log("onCapabilitiesReady", blink);
     if (capabilities.torch) {
-      
-      console.log("onCapabilitiesReady", capabilities.torch);
-      let touchBlink = true;
-      setInterval(() => {
-        track.applyConstraints({
-          advanced: [{torch: !touchBlink}]
-        }).catch(e => console.log(e));
-        touchBlink = !touchBlink;        
-      }, 500);
+      track.applyConstraints({
+        advanced: [{torch: blink}]
+      }).catch(e => console.log(e));
     }
   }
 })
