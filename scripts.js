4a521717bd67ecbbc7d678736a766e9bdd02bdc5
Patrick Bower <bower.patrick@gmail.com>
2019-01-10 11:44:37 +0000

    update url

--- scripts.js
+++ scripts.js
@@ -20,23 +20,20 @@ let APIToken = "d89724c1f1285f66151e76c547600c779272f3df7cb7124dabe1f421324bd42c
 //   console.log(event.data);
 // }
 
-// $.ajax({
-//   url: `https://api.trello.com/1/tokens/${APIToken}/webhooks/?key=${APIKey}`,
-//   type: 'DELETE',
-//   description: "clear webhook",
-//   callbackURL: "https://friendly-stonebraker-4efdef.netlify.com/",
-//   idModel: "5c1a5bc5e9a06b362599d79c",
-//   success: function(result) {
-//       console.log(result);
-//   }
-// });
+$.ajax({
+  url: `https://api.trello.com/1/tokens?webhooks=true&key=${APIKey}&token=${APIToken}`,
+  type: 'DELETE',
+  description: "clear webhook",
+  callbackURL: "https://friendly-stonebraker-4efdef.netlify.com/",
+  idModel: "5c1a5bc5e9a06b362599d79c",
+  success: function(result) {
+      console.log(result);
+  }
+});
 
 $.ajax({
   url: `https://api.trello.com/1/members/me/tokens?webhooks=true&key=${APIKey}&token=${APIToken}`,
   type: 'GET',
-  description: "setup webhook",
-  callbackURL: "https://friendly-stonebraker-4efdef.netlify.com/",
-  idModel: "5c1a5bc5e9a06b362599d79c",
   success: function(result) {
       console.log(result);
   }
